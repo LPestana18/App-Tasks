1c615b49951fa092a908cf9d16bdcb2c8de9ad54
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-08 13:52:06 -0300

    users create

--- app/src/main/java/com/example/tasks/service/repository/remote/PersonService.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/PersonService.kt
@@ -10,7 +10,19 @@ interface PersonService {
 
     @POST("Authentication/Login")
     @FormUrlEncoded
-    fun login(@Field("email") email: String ,
-              @Field("password") password: String): Call<HeaderModel>
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<HeaderModel>
+
+    @POST("Authentication/Create")
+    @FormUrlEncoded
+    fun  create(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("receiveNews") news: Boolean
+    ): Call<HeaderModel>
+
 
 }
