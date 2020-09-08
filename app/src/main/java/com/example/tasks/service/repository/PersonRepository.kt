fef3133c93dea37057f9923fd96a4d37694363ef
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-07 23:52:14 -0300

    login finished

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,5 +1,7 @@
 package com.example.tasks.service.repository
 
+import android.content.Context
+import com.example.tasks.R
 import com.example.tasks.service.HeaderModel
 import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
@@ -10,7 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository(val context: Context){
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
@@ -19,12 +21,12 @@ class PersonRepository {
         // Assíncrona
         call.enqueue(object : Callback<HeaderModel> {
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
-                listener.onFailure(t.message.toString())
+                listener.onFailure(context.getString(R.string.ERROR_LOGIN))
             }
 
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
                 if(response.code() != TaskConstants.HTTP.SUCCESS) {
-                    val validation = Gson().fromJson(response.errorBody()!!.toString(), String::class.java)
+                    val validation = Gson().fromJson(response.errorBody().toString(), String::class.java)
                     listener.onFailure(validation)
                 }else{
                     response.body()?.let { listener.onSuccess(it) }
